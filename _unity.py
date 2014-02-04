66c6d0e0768e06f2c4c37d0e07290cabb9d971df
unknown <robin.asen@gmail.com>
2014-02-04 20:43:06 +0100

    Added Windows placement commands.

--- _unity.py
+++ _unity.py
@@ -21,16 +21,37 @@ except:
     pass
 
 
-def workspace_direction(direction1, direction2=None):
+def window_direction(winDirection):
     try:
-        Key("ctrl:down, alt:down, %s/5" % direction1).execute()
-        if direction2:
-            Key("%s/5" % direction2).execute()
+        Key("ctrl:down, alt:down").execute()
+        Key("%s/5" % winDirection).execute()
     finally:  # Make sure to release the modifier keys.
         Key("alt:up, ctrl:up").execute()
 
 
-directions = {
+windowDirections = {
+    "(top|up) left": "KP_Home",
+    "(top|up)": "KP_Up",
+    "(top|up) right": "KP_Prior",
+    "left": "KP_Left",
+    "(middle|center)": "KP_Begin",
+    "right": "KP_Right",
+    "(bottom|down) left": "KP_End",
+    "(bottom|down)": "KP_Down",
+    "(bottom|down) right": "KP_Next",
+}
+
+
+def workspace_direction(wsDirection1, wsDirection2=None):
+    try:
+        Key("ctrl:down, alt:down, %s/5" % wsDirection1).execute()
+        if wsDirection2:
+            Key("%s/5" % wsDirection2).execute()
+    finally:  # Make sure to release the modifier keys.
+        Key("alt:up, ctrl:up").execute()
+
+2
+workspaceDirections = {
     "up": "up",
     "down": "down",
     "right": "right",
@@ -41,7 +62,7 @@ directions = {
 rules = MappingRule(
     mapping={
         # Commands and keywords:
-        "workspace <direction1> [<direction2>]": Function(workspace_direction),
+        "workspace <wsDirection1> [<wsDirection2>]": Function(workspace_direction),  # @IgnorePep8
         "show launcher": Key("a-f1"),
         "show hud": Key("win"),
         "toggle panel menu": Key("a-f10"),
@@ -50,27 +71,19 @@ rules = MappingRule(
         "toggle expo mode": Key("w-s"),
         # Window control
         "close window": Key("a-f4"),
-        "minimize window": Key("ca-np0"),
+        "minimize window": Key("ca-KP_Insert"),
         "maximize window": Key("cw-up"),
         "restore window": Key("cw-down"),
         # Window placement.
-        # Numpad keys doesn't seem to work?
-        "place window top left": Key("ca-np7"),
-        "place window top": Key("ca-np8"),
-        "place window top right": Key("ca-np9"),
-        "place window left": Key("ca-np4"),
-        "place window middle": Key("ca-np5"),
-        "place window right": Key("ca-np6"),
-        "place window bottom left": Key("ca-np1"),
-        "place window bottom": Key("ca-np2"),
-        "place window bottom right": Key("ca-np3"),
+        "place window <winDirection>": Function(window_direction),
 #         "": Key(""),
     },
     extras=[
         IntegerRef("n", 1, 100),
         Dictation("text"),
-        Choice("direction1", directions),
-        Choice("direction2", directions)
+        Choice("wsDirection1", workspaceDirections),
+        Choice("wsDirection2", workspaceDirections),
+        Choice("winDirection", windowDirections),
     ],
     defaults={
         "n": 1
