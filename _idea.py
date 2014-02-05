070d4b591b23871ca42efa2b4b9976292a2500a4
Kevin Menard <kevin@nirvdrum.com>
2014-02-04 22:06:21 -0500

    Automated more of the IntelliJ-based products.

--- _idea.py
+++ _idea.py
@@ -1,4 +1,4 @@
-from dragonfly import AppContext, Grammar, CompoundRule, MappingRule, Dictation, Key, Pause, Function
+from dragonfly import AppContext, Grammar, MappingRule, Dictation, Key, Pause, Function, IntegerRef, Text
 
 import lib.format
 
@@ -7,16 +7,23 @@ class CommandRule(MappingRule):
     # Code execution.
     "run app": Key("shift:down, f10, shift:up"),
     "re-run app": Key("ctrl:down, f5, ctrl:up"),
+    "run test": Key("ctrl:down, shift:down, f10, shift:up, ctrl:up"),
     "stop running": Key("ctrl:down, f2, ctrl:up"),
 
     # Code navigation.
     "open class <text>": Key("c-n/25") + Function(lib.format.pascal_case_text) + Pause("30") + Key("enter"),
     "open class chooser <text>": Key("c-n/25") + Function(lib.format.pascal_case_text) + Pause("30"),
+    "open file <text>": Key("ctrl:down, shift:down, n, shift:up, ctrl:up") + Pause("30") + Function(lib.format.camel_case_text) + Pause("30") + Key("enter"),
+    "open file chooser <text>": Key("ctrl:down, shift:down, n, shift:up, ctrl:up") + Pause("30") + Function(lib.format.camel_case_text) + Pause("30"),
+    "open symbol <text>": Key("ctrl:down, alt:down, shift:down, n, shift:up, alt:up, ctrl:up") + Pause("30") + Function(lib.format.camel_case_text) + Pause("30") + Key("enter"),
+    "open symbol chooser <text>": Key("ctrl:down, alt:down, shift:down, n, shift:up, alt:up, ctrl:up") + Pause("30") + Function(lib.format.camel_case_text) + Pause("30"),
+    "go to line": Key("c-g"),
+    "go to line <n>": Key("c-g/25") + Text("%(n)d") + Key("enter"),
     "find usages": Key("a-f7"),
     "(go to|open) declaration": Key("c-b"),
     "(go to|open) implementation": Key("ctrl:down, alt:down, b, alt:up, ctrl:up"),
     "(go to|open) super": Key("c-u"),
-    "(go to|switch to|open) test": Key("ctrl:down, shift:down, t, shift:up, ctrl:up"),
+    "(go to|switch to|open) (class|test)": Key("ctrl:down, shift:down, t, shift:up, ctrl:up"),
 
     # Project settings.
     "switch to project": Key("a-1"),
@@ -30,6 +37,7 @@ class CommandRule(MappingRule):
 
   extras = [
     Dictation("text"),
+    IntegerRef("n", 1, 50000)
   ]
 
 #class NavigationRule(CompoundRule):
