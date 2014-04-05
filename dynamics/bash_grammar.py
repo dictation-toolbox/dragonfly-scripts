096d960c37f15fad88c7986654564863c3832278
Kevin Menard <kevin@nirvdrum.com>
2014-04-05 12:54:36 -0400

    Added commands for working with iptables.

--- dynamics/bash_grammar.py
+++ dynamics/bash_grammar.py
@@ -65,6 +65,8 @@ rules = MappingRule(
         "grep recursive": Text("grep -rn ") +  Key("dquote/3, dquote/3") + Text(" *") + Key("left/3:3"),  # @IgnorePep8
         "grep recursive <text>": Text("grep -rn ") + Key("dquote/3") +  SCText("%(text)s") + Key("dquote/3") + Text(" *") + Key("left/3:3"),  # @IgnorePep8
         "ifconfig": Text("ifconfig "),
+        "(iptables|I P tables) list": Text("iptables -L"),
+        "(iptables|I P tables) flush": Text("iptables -F"),
         "jobs": Text("jobs "),
         "kill": Text("kill "),
         "kill (hard|[dash]9)": Text("kill -9 "),
