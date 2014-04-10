7229b32d232ff0575a6a8b4e23ca40018759b85f
Kevin Menard <kevin@nirvdrum.com>
2014-04-10 09:18:38 -0400

    Initial implementation of a set of proxy actions to switch between Aenea and Dragonfly actions.

---
+++ lib/dynamic_aenea.py
@@ -0,0 +1,59 @@
+import dragonfly, proxy_actions, lib.config
+config = lib.config.get_config()
+
+class ProxyBase:
+    """Base class for dynamic proxy actions. It wraps both Aenea's proxy action implementation as well
+    as the Dragonfly default.  The proxy will route calls to one of the two wrapped action classes
+    depending on whether Aenea is currently enabled."""
+
+    def __add__(self, other):
+        # __add__ is a special case.  Python won't ever call __getattr__ for __add__ and even if it did, we would
+        # want to override it.  Unlike most of the calls we proxy, which are called when the rule is executed, __add__
+        # is called when the rule is constructed.  Since the Aenea enabled state can change after rule construction,
+        # we have to execute the call on both actions so they're in the proper state for later on.
+        #
+        # Finally, dragonfly.ActionBase's implementation (the one we're ultimately delegating to) returns a new
+        # object with the chained actions, so we need to store that newly created action if we appear on the LHS
+        # of __add__.  We return `self` so if our action ends up on the RHS of an __add__ call, we maintain the
+        # proxy semantics for when the rule is actually executed.
+        self._aenea_action = self._aenea_action.__add__(other)
+        self._dragonfly_action = self._dragonfly_action.__add__(other)
+
+        return self
+
+    def __getattr__(self, attribute):
+        if config.get("aenea.enabled", False) == True:
+            return getattr(self.__dict__["_aenea_action"], attribute)
+        else:
+            return getattr(self.__dict__["_dragonfly_action"], attribute)
+
+class Key(ProxyBase):
+    def __init__(self, spec=None, static=False):
+        self._aenea_action = proxy_actions.ProxyKey(spec, static)
+        self._dragonfly_action = dragonfly.Key(spec, static)
+
+class Text(ProxyBase):
+    def __init__(self, spec=None, static=False, pause=0.02, autofmt=False):
+        self._aenea_action = proxy_actions.ProxyText(spec, static)
+        self._dragonfly_action = dragonfly.Text(spec, static, pause, autofmt)
+
+
+
+# This is a gigantic hack.  dragonfly.ActionBase performs an `isinstance` check on the supplied action to make
+# sure it is indeed an instance of dragonfly.ActionBase.  Our proxy action implementations do not inherit from
+# dragonfly.ActionBase because we would have to override every method in the inheritance hierarchy rather than
+# allowing for dynamic dispatch.  So, in order to fool isinstance, we have to override the built-in method.
+# The override specifically looks for the case where our proxy actions are compared to dragonfly.ActionBase
+# and allows for normal `isinstance` processing in all other situations.
+
+import __builtin__
+
+if not hasattr(__builtin__, "isinstance_orig"):
+    def _isinstance(instance, klass):
+        if klass is dragonfly.ActionBase and isinstance_orig(instance, ProxyBase):
+            return True
+
+        return isinstance_orig(instance, klass)
+
+    __builtin__.isinstance_orig = __builtin__.isinstance
+    __builtin__.isinstance = _isinstance
